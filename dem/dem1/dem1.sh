16e21ba5ee717beafc8c3137698b7f85bd0ca8c8
ludwMac2 <ludwikowski@gmail.com>
2022-09-12 11:08:48 +0200

    dem
    create script for downloading dem1 data

---
+++ dem/dem1/dem1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +x
+
+#http://viewfinderpanoramas.org/Coverage%20map%20viewfinderpanoramas_org3.htm
+
+URL=http://viewfinderpanoramas.org/dem1/
+
+
+for i in G H I J K L M N O P Q R S T U SA SB SC SD SE SF SG SH SI SJ SK SL SM SN
+do
+	for j in `seq -w 1 60`
+	do
+		final_file=${i}${j}.zip
+		final_url=${URL}${final_file}
+		if ! [ -f $final_file ]; then
+			curl -I -s ${final_url} | grep -q "Content-Type: application/zip" && echo curl -LO ${final_url}
+		fi
+	done
+done
