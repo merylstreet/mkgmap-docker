144304b5d23251a8fea1ac3862c2d6e48e512117
ludw@trytb <ludwikowski@gmail.com>
2022-09-18 17:05:45 +0200

    some defaults for creating maps

--- create_map.sh
+++ create_map.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker run -it --rm -v $(pwd):/data \
-    -e MAX_HEAP_SIZE=14G \
+    -e MAX_HEAP_SIZE=18G \
     -e FAMILY_NAME=Poland \
     -e DESCRIPTION="PL by Ludw" \
     -e PRODUCT_VERSION="2209" \
     -e MAP_NAME=63240903 \
     -e STYLE=mkgmap \
-    -e TYP=mkgmap \
+    -e TYP=opentopomap \
     ludw/mkgmap
